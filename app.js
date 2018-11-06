07babeefe2276f5691ee35e29868d29559c34254
Ivy Leigh Powell <icp5@txstate.edu>
2018-11-05 19:14:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //using express.js just because it's relatively user friendly
 const express = require('express')
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
 
 //making the express object that will be used to control our server
 const app = express()
@@ -23,6 +27,22 @@ app.use(express.static('src'))
 // viewed at http://localhost:8080 on local machines
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/html/index.html'));
+});
+
+golbal.db = db;
+
+// configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(fileUpload()); // configure fileupload
+
+// set the app to listen on the port
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
 
 
