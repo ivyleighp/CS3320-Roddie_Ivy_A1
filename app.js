3e32ca44aadad070ad68f6c6fcb87bc703e58d19
Ivy Leigh Powell <icp5@txstate.edu>
2018-11-30 15:44:14 -0600

    Revert "Update app.js"
    This reverts commit 466083487a3b87f5d43bf2606d4f35d5f5a84018.

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.listen(port, () => console.log(`App listening on port ${port}!`))
 app.use(function (req, res, next) {
   res.status(404).send("Sorry, page doesn't exist!")
 })
-/*
+
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
@@ -78,9 +78,7 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 });
 
 connection.end();
-*/
 
-/*
 // configure middleware
 app.set('port', process.env.port || port); // set express to use this port
 app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
@@ -112,5 +110,3 @@ app.post('/editQ/:quoteId', editQuote);
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
-
-*/
