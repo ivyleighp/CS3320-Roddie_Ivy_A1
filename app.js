7b86778f19a75efea2965945d62858e32f02fb07
Ivy Leigh Powell <icp5@txstate.edu>
2018-11-30 15:44:21 -0600

    Revert "update"
    This reverts commit a238d389aa7a760e951fe0a67d8374e8a8a89848.

--- app.js
+++ app.js
@@ -33,38 +33,14 @@ const db = mysql.createConnection ({
 global.db = db; */
 
 
+var baseUrl = "https://a1-ivy-roddie.herokuapp.com/"
 
-
-
-var path = require('path');
-
-// // set the port of our application
-// // process.env.PORT lets the port be set by Heroku
-// // this is necessary because at deploy time, Heroku decides what port the application runs on.
-var port = process.env.PORT || 8080;
-
-//telling express to serve everything in the public directory so we can use it on the page
-//public contains html and css folders/code
-app.use(express.static('public'))
-//same thing for src directory, to serve javascript
-app.use(express.static('src'))
-
-//basic response for the homepage, basic url '/'', sending the main.html file, which loads the
-//CSS and JS in /public/ and /src/
-// viewed at http://localhost:8080 on local machines
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/indexClient.ejs'));
+const herokuClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-
-//telling the server to listen on the assigned port
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-
-// a 404 page just cause
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry, page doesn't exist!")
-})
+herokuClient.connect();
 
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
